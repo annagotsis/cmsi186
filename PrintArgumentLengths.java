b94635332b9a7c842bb3d6256dea886fc792b8d0
Anna Gotsis <agotsis@lion.lmu.edu>
2015-01-22 17:51:21 -0800

    Added for loop

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,9 +1,11 @@
 public class PrintArgumentLengths {
 	public static void main(String[] args){
 		if (args.length == 0) {
-			System.out.println("Usage: java PrintArgumentLengths <any number of arguments>");
-        		 return;
+		System.out.println("Usage: java PrintArgumentLengths <any number of arguments>");
+            return;
 		}
-
+    for (String arg: args) {
+    	
+    }
 	}
 }
