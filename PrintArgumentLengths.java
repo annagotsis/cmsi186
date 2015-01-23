457117f4c8848b2327787b7821562e1b491a0c6c
Anna Gotsis <agotsis@lion.lmu.edu>
2015-01-22 17:45:54 -0800

    Added code to print out the arguments

--- PrintArgumentLengths.java
+++ PrintArgumentLengths.java
@@ -1,5 +1,6 @@
 public class PrintArgumentLengths {
 	public static void main(String[] args){
-		
+		System.out.println("Usage: java PrintArgumentLengths <any number of arguments>");
+            return;
+		}
 	}
-}
