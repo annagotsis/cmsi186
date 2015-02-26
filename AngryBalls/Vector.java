7cb92dd93900388723f558d7083d52fe4f22c124
Anna Gotsis <agotsis@lion.lmu.edu>
2015-02-26 15:36:26 -0800

    First Commit

---
+++ AngryBalls/Vector.java
@@ -0,0 +1,30 @@
+public class Vector {
+
+    // Declare my instance variables!
+    
+    public Vector(double x, double y) {
+        // Impelemnt me!
+
+    } 
+    
+    public double x() {
+        // Implement me!
+        return 0;
+    }
+    
+    public double y() {
+        // Implement me!
+        return 0;
+    }
+
+    public Vector add(Vector v) {
+        // Implement me!
+        return null;
+    }
+
+    public Vector scale(double magnitude) {
+        // Implement me!
+        return null;
+    }
+
+}
