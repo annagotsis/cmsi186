53d62d8e4fda42df1707c1559085c316a1cc8eb8
Anna Gotsis <agotsis@lion.lmu.edu>
2015-02-26 15:37:17 -0800

    First Commit

---
+++ AngryBalls/Ball.java
@@ -0,0 +1,25 @@
+public class Ball {
+
+    // Declare my instance variables!
+    
+    public Ball(double radius, Vector location, Vector initialVelocity) {
+        // Implement me!
+    }
+
+    public Vector getLocation() {
+        // Implement me!
+    }
+
+    public double getRadius() {
+        // Implement me!
+    }
+    
+    public void accelerate(Vector acceleration, double grain) {
+        // Implement me!
+    }
+
+    public void move(double grain) {
+        // Implement me!
+    }
+
+}
