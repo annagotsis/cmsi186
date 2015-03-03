24d7ea3a59c52d7b9e3ac68a0838261337fb15de
Anna Gotsis <agotsis@lion.lmu.edu>
2015-03-03 15:22:22 -0800

    Instance variables declared, constructor almost done

--- AngryBalls/Ball.java
+++ AngryBalls/Ball.java
@@ -1,17 +1,28 @@
 public class Ball {
 
     // Declare my instance variables!
+    private double radius;
+    private Vector location;
+    private Vector initialVelocity;
+    private Vector acceleration;
+    private double grain;
     
     public Ball(double radius, Vector location, Vector initialVelocity) {
         // Implement me!
+        this.radius = radius; 
+        this.location = location; 
+        this.initialVelocity = initialVelocity;
+        Ball[] balls = new Ball[2];
     }
 
     public Vector getLocation() {
         // Implement me!
+        return this.location;
     }
 
     public double getRadius() {
         // Implement me!
+        return this.radius;
     }
     
     public void accelerate(Vector acceleration, double grain) {
