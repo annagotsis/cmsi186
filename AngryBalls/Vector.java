18d8d20cf0b282222c132047e65aa55790bfb110
Anna Gotsis <agotsis@lion.lmu.edu>
2015-03-03 15:21:34 -0800

    Instance variables defined

--- AngryBalls/Vector.java
+++ AngryBalls/Vector.java
@@ -1,29 +1,42 @@
 public class Vector {
-
     // Declare my instance variables!
+        private double x;
+        private double y;
+        private Vector v;
+        private double magnitude;
     
     public Vector(double x, double y) {
         // Impelemnt me!
-
+        this.x = x;
+        this.y = y;
+        Vector v = new Vector(x,y);
     } 
     
     public double x() {
         // Implement me!
-        return 0;
+        return x;
     }
     
     public double y() {
         // Implement me!
-        return 0;
+        return y;
     }
 
     public Vector add(Vector v) {
         // Implement me!
+        //Vector v = new Vector(a,b),
+        //       w = new Vector(c,d),
+        //      sum= v.add(w);
+
+
         return null;
     }
 
     public Vector scale(double magnitude) {
         // Implement me!
+        // take in the magnitude (.5)
+        // and creates a new vector with the values multiplied by .5
+        // v1.x.scale()
         return null;
     }
 
