0401d00fede6daccc31563ab04ee05dc5224b8c8
Anna Gotsis <agotsis@lion.lmu.edu>
2015-03-03 16:14:41 -0800

    variables declared, some parts finished

--- AngryBalls/Ball.java
+++ AngryBalls/Ball.java
@@ -12,25 +12,26 @@ public class Ball {
         this.radius = radius; 
         this.location = location; 
         this.initialVelocity = initialVelocity;
-        Ball[] balls = new Ball[2];
     }
 
     public Vector getLocation() {
         // Implement me!
-        return this.location;
+        return location;
     }
 
     public double getRadius() {
         // Implement me!
-        return this.radius;
+        return radius;
     }
     
     public void accelerate(Vector acceleration, double grain) {
         // Implement me!
+        
     }
 
     public void move(double grain) {
         // Implement me!
+        // velocity changing your location
     }
 
 }
