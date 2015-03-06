6e021ecd02e29c307e6b0baeeb604f64825ecbe2
Anna Gotsis <agotsis@lion.lmu.edu>
2015-03-05 16:14:38 -0800

    Accelerate and move complete, main method started

--- AngryBalls/Ball.java
+++ AngryBalls/Ball.java
@@ -26,12 +26,51 @@ public class Ball {
     
     public void accelerate(Vector acceleration, double grain) {
         // Implement me!
-        
+       this.initialVelocity = this.initialVelocity.add(acceleration.scale(grain));
     }
 
     public void move(double grain) {
         // Implement me!
-        // velocity changing your location
+        this.location = this.location.add(acceleration.scale(grain));
     }
 
+    public static void main() {
+        double redBallRadius = 0;
+        double redBallX = 0;
+        double redBallY = 0;
+        double redBallVelocityX = 0;
+        double redBallVelocityY = 0;
+        double blueBallRadius = 0;
+        double blueBallX = 0;
+        double blueBallY = 0;
+        double blueBallVelocityX = 0;
+        double blueBallVelocityY = 0;
+        double grain = 0;
+
+        if (args.length == 0) {
+                System.out.println("Usage: java AngryBallsSimulation <redradius> <red x> <red y> <red velocity x>" +
+                "<red velocity y> <blue radius> <blue x> <blue y> <blue velocity x> <bluevelocity y> [ <grain> ]" + 
+                    
+                "All sizes are in meters and the grain is in seconds. The grain is optional and defaults to 1 second if not supplied."); 
+            return;
+
+       try { 
+        double redBallRadius = Double.parseDouble[1];
+        double redBallX = Double.parseDouble[2];
+        double redBallY = Double.parseDouble[3];
+        double redBallVelocityX = Double.parseDouble[4];
+        double redBallVelocityY = Double.parseDouble[5];
+        double blueBallRadius = Double.parseDouble[6];
+        double blueBallX = Double.parseDouble[7];
+        double blueBallY = Double.parseDouble[8];
+        double blueBallVelocityX = Double.parseDouble[9];
+        double blueBallVelocityY = Double.parseDouble[10];
+        double grain = Double.parseDouble[11];
+
+        double radius = redBallRadius;
+        } catch { 
+            System.out.println("The arguments supplied do not match whatAngryBallsSimulation expects.");
+            }
+        }
+    }
 }
