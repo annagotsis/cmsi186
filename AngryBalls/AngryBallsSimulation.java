6681e18ba6ca6abda41efe0d2317e40d9e5a3b17
Anna Gotsis <agotsis@lion.lmu.edu>
2015-03-10 16:12:24 -0700

    2 balls created

---
+++ AngryBalls/AngryBallsSimulation.java
@@ -0,0 +1,19 @@
+public class AngryBallsSimulation {
+
+
+	public AngryBallsSimulation() {
+		if (args.length = 10) {
+
+	double redBallRadius = (Double.parseDouble(args[1]));
+	double redBallLocation = (Double.parseDouble(args[2]), Double.parseDouble(args[3]));
+	double redBallVelocity = (Double.parseDouble(args[4]), Double.parseDouble(args[5]));
+	double blueBallRadius = (Double.parseDouble(args[6]));
+	double blueBallLocation = (Double.parseDouble(args[7]), Double.parseDouble(args[8]));
+	double blueBallVelocity = (Double.parseDouble(args[9]), Double.parseDouble(args[10]);
+	
+		Ball red = new Ball(redBallRadius, redBallLocation, redBallVelocity);
+		Ball blue = new Ball(blueBallRadius, blueBallLocation, blueBallVelocity);
+
+	}
+}
+
