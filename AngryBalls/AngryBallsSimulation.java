3ad83e72a6214b92810a25c534b5445eb736d4e7
Anna Gotsis <agotsis@lion.lmu.edu>
2015-03-17 15:03:46 -0700

    Final commit

--- AngryBalls/AngryBallsSimulation.java
+++ AngryBalls/AngryBallsSimulation.java
@@ -6,12 +6,13 @@ public class AngryBallsSimulation {
 
 	public static String usageError = "Usage: java AngryBallsSimulation <redradius> <red x> <red y> <red velocity x>" +
                 "<red velocity y> <blue radius> <blue x> <blue y> <blue velocity x> <bluevelocity y> [ <grain> ]";
-        public static String wrongArguments = "The arguments supplied do not match what AngryBallsSimulation expects.";
-        public static String sizes = "All sizes are in meters and the grain is in seconds. The grain is optional and defaults to 1 second if not supplied.";
+    public static String wrongArguments = "The arguments supplied do not match what AngryBallsSimulation expects.";
+    public static String sizes = "All sizes are in meters and the grain is in seconds. The grain is optional and defaults to 1 second if not supplied.";
              
 	
 
-	public AngryBallsSimulation(double redBallRadius, double redBallX, double redBallY, double redBallVelocityX, double redBallVelocityY, double blueBallRadius, double blueBallX, double blueBallY, double blueBallVelocityX, double blueBallVelocityY, double grain){
+	public AngryBallsSimulation(double redBallRadius, double redBallX, double redBallY, double redBallVelocityX, double redBallVelocityY, double blueBallRadius, double blueBallX, double blueBallY, double blueBallVelocityX, double blueBallVelocityY, double grain) {
+		
 		Ball red = new Ball(redBallRadius, new Vector(redBallX, redBallY) , new Vector(redBallVelocityX, redBallVelocityY)); 
 		Ball blue = new Ball(blueBallRadius, new Vector(blueBallX, blueBallY), new Vector(blueBallVelocityX, blueBallVelocityY));
 		grain = grain;
@@ -25,41 +26,43 @@ public class AngryBallsSimulation {
 
 
 	public boolean collision (Ball R, Ball B) {
-            if ((( Math.sqrt(((R.getLocation().x() - B.getLocation().x()) * (R.getLocation().x() - B.getLocation().x())) - ((R.getLocation().y() - B.getLocation().y()) * (R.getLocation().y() - B.getLocation().y())))) - B.getRadius() - R.getRadius())  <= 0) {
-                return true;
-            } else {
-                return false;
-            }
-       } 
+       //if (this.getRadius()
+       //if (distance formula) is less than one then return true
+        if (((Math.sqrt(((R.getLocation().x() - B.getLocation().x()) * (R.getLocation().x() - B.getLocation().x())) - ((R.getLocation().y() - B.getLocation().y()) * (R.getLocation().y() - B.getLocation().y())))) - B.getRadius() - R.getRadius())  <= 0) {
+            return true;
+        } else {
+            return false;
+       }
+    } 
 	
 	public static void main(String[] args) {
 		boolean argumentsValid = true;
 		double[] pArgs = new double[11];
 
 		if (args.length == 10) {
-    	           try {
-            	      for (int i = 0; i < args.length; i++) { 
-                        pArgs[i] = Double.parseDouble(args[i]);
-                      }
-                   } catch (NumberFormatException e) {
-                       System.out.println(wrongArguments + "\n" + usageError + "\n" + sizes);
-                   }
+    		try {
+            	for (int i = 0; i < args.length; i++) { 
+                    pArgs[i] = Double.parseDouble(args[i]);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println(wrongArguments + "\n" + usageError + "\n" + sizes);
+            }
 
-		grain = 1.0;
-		AngryBallsSimulation sim = new AngryBallsSimulation(pArgs[0], pArgs[1], pArgs[2], pArgs[3], pArgs[4], pArgs[5], pArgs[6], pArgs[7], pArgs[8], pArgs[9], grain);
-                sim.simulation(redBall, blueBall, grain);
+			grain = 1.0;
+			AngryBallsSimulation sim = new AngryBallsSimulation(pArgs[0], pArgs[1], pArgs[2], pArgs[3], pArgs[4], pArgs[5], pArgs[6], pArgs[7], pArgs[8], pArgs[9], grain);
+            sim.simulation(redBall, blueBall, grain);
 		
 		} else if (args.length == 11) {
-		    try {
-            	         for (int i = 0; i < args.length; i++) { 
-                          pArgs[i] = Double.parseDouble(args[i]);
-                    }
-                    } catch (NumberFormatException e) {
-                        System.out.println(wrongArguments + "\n" + usageError + "\n" + sizes);            }
+			try {
+            	for (int i = 0; i < args.length; i++) { 
+                    pArgs[i] = Double.parseDouble(args[i]);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println(wrongArguments + "\n" + usageError + "\n" + sizes);            }
 
-		grain = pArgs[10];
-		AngryBallsSimulation sim = new AngryBallsSimulation(pArgs[0], pArgs[1], pArgs[2], pArgs[3], pArgs[4], pArgs[5], pArgs[6], pArgs[7], pArgs[8], pArgs[9], grain);
-                sim.simulation(redBall, blueBall, grain);
+			grain = pArgs[10];
+			AngryBallsSimulation sim = new AngryBallsSimulation(pArgs[0], pArgs[1], pArgs[2], pArgs[3], pArgs[4], pArgs[5], pArgs[6], pArgs[7], pArgs[8], pArgs[9], grain);
+            sim.simulation(redBall, blueBall, grain);
 		
 		} else if (args.length == 0) {
 			System.out.println(usageError + "\n" + sizes);
@@ -83,24 +86,25 @@ public class AngryBallsSimulation {
 			blueBall.accelerate(new Vector (0, -9.8), grain);
 			t += 1;
 
-		if ((collision(blueBall, redBall))) {
-                collided = true;
+			if ((collision(blueBall, redBall))) {
+              collided = true;
             }
 
-		if (redBall.getLocation().y() <= 0 && blueBall.getLocation().y() <= 0) {
-			System.out.println("end");
-		}
+			if (redBall.getLocation().y() <= 0 && blueBall.getLocation().y() <= 0) {
+				System.out.println("end");
+			}
 
-		if ((simul) && (collided)) {
-			System.out.println("end");
-			System.out.println("The balls collided at timestamp" + t + "with the red ball at " + redBall.getLocation() + "and the blue ball at "+ blueBall.getLocation());
+			if ((simul) && (collided)) {
+				System.out.println("end");
+				System.out.println("The balls collided at timestamp" + t + "with the red ball at " + redBall.getLocation() + "and the blue ball at "+ blueBall.getLocation());
 
+			}
+			if ((!collided) && (!simul)) {
+				System.out.println("end");
+				System.out.println("The balls did not collide");
+			}
 		}
-		if ((!collided) && (!simul)) {
-			System.out.println("end");
-			System.out.println("The balls did not collide");
-		}
-	}
+	}	
 }
 
 
