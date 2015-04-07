6e6897f98eb6cc361e518b02353dff6b1e93366d
Anna Gotsis <agotsis@lion.lmu.edu>
2015-04-07 14:15:08 -0700

    Update Intmonster.java

--- Intmonster.java
+++ Intmonster.java
@@ -1,7 +1,7 @@
 public class Intmonster {
 	
-	private byte[] byteArray;
 	private int sign;
+	private int[] mag;
 
 
 	public Intmonster() {
@@ -12,31 +12,44 @@ public class Intmonster {
 		this(s);
 	}
 
-    public void convertStringToByteArray(){
+    public void convertStringToIntArray(){
 		String stringToConvert = this(s);
-		byte[] byteArray = stringToConvert.getBytes();
-		System.out.println(byteArray.length);
+		int[] intArray = new int[stringToConvert.length];
+		for (int i = 0; i < stringToConvert.length; i++) {
+			intArray[i] = Integer.parseInt(stringToConvert[i]);
+		}
 	}
 	
-	public String toBinary(Intmonster n) {
-		if (n == 0) {
-			return "0";
-		}
-		String binary = " ";
-		while (n > 0) {
-			int rem = n % 2;
-			binary = rem + binary;
-			n = n / 2;
-		}
-		return binary;
-	}
+	// public String toBinary(Intmonster n) {
+	// 	if (n == 0) {
+	// 		return "0";
+	// 	}
+	// 	String binary = " ";
+	// 	while (n > 0) {
+	// 		int rem = n % 2;
+	// 		binary = rem + binary;
+	// 		n = n / 2;
+	// 	}
+	// 	return binary;
+	// }
 
 	public boolean equals(Object n) {
-		if (this.equals(n)) { //(n == this)
+		if (this == n) { 
 			return true;
-		} else {
+		}if (n == null) {
+			return false;
+		}if (!(n instanceof Intmonster)) {
 			return false;
 		}
+		Intmonster other = (Intmonster)n;
+		
+		if(!(this.toString().equals(other.toString()))) {
+			return false;
+		} for (int i=0; i < mag.length; i++){
+			if (nInt.mag[i] != mag[i]) {
+				return false
+			}
+		 } return true;return true;
 	}
 
 	public boolean isGreaterThan(Intmonster n) {
@@ -56,21 +69,43 @@ public class Intmonster {
 	}
 
 	public Intmonster plus(Intmonster addend) {
-		int sum = 0; //int[] a = {}; int sum = IntStream.of(a).sum();
-		for (string addend : args)
-			sum += Integer.parseInt(addend);
-		System.out.println(sum);
+		// int sum = 0; //int[] a = {}; int sum = IntStream.of(a).sum();
+		// for (string addend : args)
+		// 	sum += Integer.parseInt(addend);
+		// System.out.println(sum);
+
+		int [] result;
+		if (addend.sign == 0)
+			return this;
+		if (sign == 0)
+			return addend;
+		if (addend.sign == sign)
+			return new Intmonster(plus(mag, addend.mag), sign);
+
+		//System.out.print(a.charAt(0) + a.charAt(1) + a.charAt(2) + a.charAt(3) + a.charAt(4) + a.charAt(5) + a.charAt(6) + a.charAt(7) + a.charAt(8) + a.charAt(9));
 	}
 
 	public Intmonster minus(Intmonster subtrahend) {
-		Integer.parseInt(subtrahend.toString());
-		System.out.println(this.toString() - subtrahend);
+		// Integer.parseInt(subtrahend.toString());
+		// System.out.println(this.toString() - subtrahend);
+		int [] result;
+		if (subtrahend.sign == 0)
+			return this;
+		if (sign == 0)
+			return subtrahend;
+		if (subtrahend.sign == sign)
+			return new Intmonster(minus(mag, subtrahend.mag), sign);
 
 	}
 
 	public Intmonster times(Intmonster factor) {
-		int product = this.toString() * factor.toString();
-		System.out.println(product);
+		// int product = this.toString() * factor.toString();
+		// System.out.println(product);
+		if (factor.sign == 0 || sign == 0)
+			return 0;
+
+		int[] result = multiply(mag, mag.length, factor.mag, factor.mag.length, null)
+			return result;
 	}
 
 	public Intmonster div(Intmonster divisor) {
@@ -83,14 +118,14 @@ public class Intmonster {
 	}
 
 	public Intmonster mod(Intmonster divisor) {
-		//if (m.sign <= 0) {
+		/*if (divisor.sign <= 0) {
 
-		//} throw Arithmetic Exception ae("n")
+		} throw Arithmetic Exception ae("n")
 
-		//return result = this.remainder(m);
+		return result = this.remainder(divisor); */
 	}
 
 	public static void main(String[] args) {
-		new Main().convertStringToByteArray();
+		new Main().convertStringToIntArray();
 	}
 }
