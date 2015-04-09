c08bdde27179753f9f85d9ade79f3ef86330d32c
Anna Gotsis <agotsis@lion.lmu.edu>
2015-04-09 15:48:57 -0700

    Complete version

---
+++ MonteCarlo/PiEstimator.java
@@ -0,0 +1,30 @@
+public class PiEstimator {
+
+	public static void main(String[] args) {
+	
+	int successfulThrows = 0;
+	int numberOfThrows = 10000;
+	
+	if (args.length == 1) {
+		numberOfThrows = Integer.parseInt(args[0]);
+	}
+		System.out.println("start");
+
+		for (int i = 0; i < numberOfThrows; i++) {
+			
+			double x = Math.random();
+			double y = Math.random();
+			
+			if ((x*x) + (y*y) <= 1) {
+				successfulThrows++;
+
+				System.out.println(x + " " + y + " in");
+			} else {
+				System.out.println(x + " " + y + " out");
+			}	
+		}
+
+		System.out.println("end");
+		System.out.println("Darts: " + numberOfThrows + " Hits: " + successfulThrows + " Pi Estimate: " + 4*((double)successfulThrows/(double)numberOfThrows));
+	}
+}
